7b096ad61c79aae2535e080bc101c611239eb907
McGregorSt <stasiak.gregor@gmail.com>
2021-01-23 18:51:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn ReactVile steel his known condole shamed hope memory through
+        misery soils, muse might felt to alas the his, to hellas.
+      </a>
+    </header>
+  </div>
+);
+
+export default App;
